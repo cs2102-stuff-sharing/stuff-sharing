46830614a8371a304fb32e3677d0b8c109d8cb47
HooDeLin <hoodelin0619@gmail.com>
2016-02-28 22:12:32 +0800

    Update sign up with proper fields

--- signup.php
+++ signup.php
@@ -1,47 +1,48 @@
 <?php
-include('db.php');
-if(isset($_POST['action']))
-{          
-    if($_POST['action']=="signup")
-    {
-        $name       = pg_escape_string($connection,$_POST['name']);
-        $email      = pg_escape_string($connection,$_POST['email']);
-        $password   = pg_escape_string($connection,$_POST['password']);
-        $query = "SELECT email FROM users where email='".$email."'";
-        $result = pg_query($connection,$query);
-        $numResults = pg_num_rows($result);
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) // Validate email address
-        {
-            $message =  "Invalid email address please type a valid email!!";
-        }
-        elseif($numResults>=1)
-        {
-            $message = $email." Email already exist!!";
-        }
-        else
-        {
-            pg_query("insert into users(name,email,password) values('".$name."','".$email."','".md5($password)."')");
-            $message = "Signup Sucessfully!!";
-        }
-    }
-}
-
+  include('db.php');
+  if(isset($_POST['action']))
+  {          
+      if($_POST['action']=="signup")
+      {
+          $firstName = pg_escape_string($connection,$_POST['firstName']);
+          $lastName = pg_escape_string($connection,$_POST['lastName']);
+          $dob = pg_escape_string($connection,$_POST['dob']);
+          $email = pg_escape_string($connection,$_POST['email']);
+          $password = pg_escape_string($connection,$_POST['password']);
+          $query = "SELECT email FROM users where email='".$email."'";
+          $result = pg_query($connection,$query);
+          $numResults = pg_num_rows($result);
+          if (!filter_var($email, FILTER_VALIDATE_EMAIL)) // Validate email address
+          {
+              $message =  "Invalid email address please type a valid email!!";
+          }
+          elseif($numResults>=1)
+          {
+              $message = $email."Email already exist!!";
+          }
+          else
+          {
+              pg_query($connection,"insert into users(firstName,lastName,dob,email,password) values('".$firstName."','".$lastName."','".$dob."','".$email."','".md5($password)."')");
+              $message = "Signup Sucessfully!!";
+          }
+      }
+  }
 ?>
-<!-- Login and Signup forms -->
 <?php 
-	if(isset($message))
-	{
-		echo $message;
-	}
-	else
-	{
-		echo "message is unset";
-	} 
+  if(isset($message))
+  {
+    echo $message;
+  }
+  else
+  {
+    echo "<p>Sign up here by filling up the form</p>";
+  } 
 ?>
-
 <div id="tabs-2">
   <form action="signup.php" method="post">
-  <p><input id="name" name="name" type="text" placeholder="Name"></p>
+  <p><input id="first-name" name="firstName" type="text" placeholder="First Name"></p>
+  <p><input id="last-name" name="lastName" type="text" placeholder="Last Name"></p>
+  <p><input id="dob" name="dob" type="date"></p>
   <p><input id="email" name="email" type="text" placeholder="Email"></p>
   <p><input id="password" name="password" type="password" placeholder="Password">
   <input name="action" type="hidden" value="signup" /></p>
