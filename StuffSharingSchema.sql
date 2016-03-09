1ffdf8f39c27c0fc3df105b4a17a8a2495056566
HooDeLin <hoodelin0619@gmail.com>
2016-03-09 15:09:04 +0800

    Updated schema based on previous meeting

--- StuffSharingSchema.sql
+++ StuffSharingSchema.sql
@@ -1,34 +1,51 @@
 CREATE table Users (
-firstName varchar(64) NOT NULL,
-lastName varchar(64) NOT NULL,
-password varchar(64) NOT NULL,
-dob DATE NOT NULL,
-email varchar(256) PRIMARY KEY,
-reputation INT);
+    firstName VARCHAR(64) NOT NULL,
+    lastName VARCHAR(64) NOT NULL,
+    password VARCHAR(64) NOT NULL,
+    dob DATE NOT NULL,
+    email VARCHAR(256) PRIMARY KEY,
+    userPoint INT DEFAULT 100,
+    blackListCount INT DEFAULT 0
+);
 
 CREATE table Categories (
-category varchar(64) PRIMARY KEY);
+    category VARCHAR(64) PRIMARY KEY
+);
 
-CREATE table itemList (
-itemID SERIAL PRIMARY KEY,
-ownerEmail varchar(256) REFERENCES Users(email),
-itemName varchar(64),
-itemDescription varchar(256),
-itemAvailability varchar(64),
-itemCategory varchar(64) REFERENCES Categories(category));
-
-CREATE table record (
-recordID SERIAL PRIMARY KEY,
-borrowerEmail varchar(256) REFERENCES Users(email),
-itemID INT REFERENCES itemList(itemID),
-fee INT,
-timeToReturn DATE,
-time TIME,
-successful BOOLEAN);
+/* When a user deletes an item, it still preserves in the database so that the notifications can still be sent properly */
+CREATE table ItemList (
+    itemId SERIAL PRIMARY KEY,
+    ownerEmail VARCHAR(256) REFERENCES Users(email),
+    itemName VARCHAR(64),
+    itemDescription VARCHAR(256),
+    itemDeleted BOOLEAN DEFAULT FALSE,
+    itemCategory VARCHAR(64) REFERENCES Categories(category)
+);
 
 CREATE TABLE Advertise (
-ownerEmail varchar(256) REFERENCES Users(email),
-itemID INT REFERENCES itemList(itemID),
-statement varchar(256),
-price INT,
-location varchar(256));
+    itemId SERIAL PRIMARY KEY REFERENCES itemList(itemId),
+    minimumBidPoint INT NOT NULL
+);
+
+/* All current biddings will be recorded in this table, after the bidding ends, all the bidding records (successful or not) will be removed */
+CREATE TABLE BiddingList (
+    itemId SERIAL REFERENCES itemList(itemId),
+    bidderId VARCHAR(256) REFERENCES Users(email),
+    bidAmount INT CHECK (bidAmount > 0),
+    PRIMARY KEY (itemId, bidderId)
+);
+
+/* This table records all the successful biddings, the record will be deleted after the transaction is done */
+CREATE TABLE Record (
+    itemId SERIAL REFERENCES itemList(itemId),
+    bidderId VARCHAR(256) REFERENCES Users(email),
+    bidAmount INT CHECK (bidAmount > 0),
+    PRIMARY KEY (itemId, bidderId)
+);
+
+CREATE TABLE Notifications (
+    receiver VARCHAR(256) REFERENCES Users(email),
+    status VARCHAR(64) NOT NULL,
+    productId SERIAL NOT NULL,
+    PRIMARY KEY (receiver, productId)
+);
