7d2036d4291c5b376e5dec18036da82bff51477b
Dixon Lam <dixonlam1314@gmail.com>
2016-03-11 01:19:34 +0800

    Implement audited

---
+++ addItem.php
@@ -0,0 +1,73 @@
+<?php
+  session_start();
+
+  include('db.php');
+  include('header.php');
+  if(!isset($_SESSION['key']))
+  {
+  	header("Location: /stuff-sharing/login.php?error=NOT_LOGIN");
+  }
+  elseif(isset($_POST['action']) && $_POST['action'] == "addItem")
+  {
+  	$itemName = pg_escape_string($connection,$_POST['itemName']);
+  	$itemDescription = pg_escape_string($connection,$_POST['itemDescription']);
+  	$itemCategory = pg_escape_string($connection,$_POST['itemCategory']);
+
+  	$addItemQuery = "insert into ItemList(itemName,itemDescription,itemCategory) values ('"
+  		. $itemName ."','" . $itemDescription ."', '" .$itemCategory."')";
+	$addItemResult = pg_query($connection, $addItemQuery);
+
+	if($addItemResult){
+
+		echo ("Item Added!!");
+	}
+  }
+
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-4 col-md-offset-4">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Add Item Page</h3>
+        </div>
+        <div class="panel-body">
+          <?php
+            if(isset($message))
+            {
+              echo '<div class="alert alert-danger" role="alert">
+                      <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                      <span class="sr-only">Error:</span>' . 
+                      $message .
+                    '</div>';
+            }
+          ?>
+          <form action="addItem.php" method="post">
+            <fieldset>
+              <div class="form-group">
+                <input class="form-control" id="item-name" name="itemName" type="text" placeholder="Item Name" autofocus>
+              </div>
+              <div class="form-group">
+                <input class="form-control" id="item-description" name="itemDescription" type="text" placeholder="Item Description">
+              </div>
+              <div class="form-group">
+                <select class="form-control" id="categories" name="itemCategory">
+                	<option value = 'Home'>Home Use</option>
+                    <option value = 'Phone'>Phone</option>
+                    <option value = 'School'>School Use</option>
+                    <option value = 'Personal'>Personal Use</option>
+                    <option value = 'Other'>Other</option>
+              </div>
+              <div class="form-group">
+                <input class="form-control" name="action" type="hidden" value="addItem" />
+              </div>
+              <button type="submit" class="btn btn-success btn-block">addItem</button>
+
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
