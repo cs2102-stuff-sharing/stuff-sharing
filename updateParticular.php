78d929c92a07803d8f3f22d57414fdfc48f074d7
Junhan Feng <junhanfeng@gmail.com>
2016-03-22 16:32:09 +0800

    Added updated particulars

---
+++ updateParticular.php
@@ -0,0 +1,129 @@
+<?php
+  session_start();
+
+  include('db.php');
+  include('header.php');
+  if(!isset($_SESSION['key']))
+  {
+  	header("Location: /stuff-sharing/login.php?error=NOT_LOGIN");
+  }
+  else
+  {
+	$email = pg_escape_string($connection,$_SESSION['key']);
+    $query = "SELECT firstname, lastname FROM users where email='".$email."'";
+    $result = pg_query($connection,$query) or die('Query failed:'.pg_last_error());
+    $row = pg_fetch_row($result);
+  }
+	
+	if(isset($_SESSION['updateid']))
+	{
+		$updateid = pg_escape_string($connection,$_SESSION['updateid']);
+		$updateResult = pg_query($connection,"SELECT u.firstname, u.lastname, u.password, u.dob, u.email 
+		FROM users u WHERE u.email='".$email."'");
+		if(!$updateResult)
+		{
+		$row = pg_fetch_row($updateResult);			
+			$message = "Something seems to be wrong, please try later ";
+		}
+		else
+		{
+			$row = pg_fetch_row($updateResult);
+			$userFName = $row[0];
+			$userLName = $row[1];
+			$userPassword = $row[2];
+			$userDOB = $row[3];			
+			$userEmail = $row[4];
+		}
+	}
+
+  if(isset($_POST['action']) && $_POST['action'] == "updateid")
+  {  
+  	$newFName = pg_escape_string($connection,$_POST['userFName']);
+	$newLName = pg_escape_string($connection,$_POST['userLName']);
+	$newPassword = pg_escape_string($connection,$_POST['userPassword']);
+	$newDOB = pg_escape_string($connection,$_POST['userDOB']);
+	
+  	$updateIDQuery = "UPDATE users SET firstname='".$newFName."', lastname='".$newLName."', password='".$newPassword."', dob='".$newDOB."'
+		WHERE email = '".$userEmail."'";
+	  $updateIDResult = pg_query($connection, $updateIDQuery);
+
+		if($updateIDResult){
+			echo ("
+			<div class=\"alert alert-info\">
+				<a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a>
+				particulars updated!
+			</div>
+					");
+		
+		}
+				
+		//header("Refresh: 10; Location: /stuff-sharing/updateParticular.php");
+		//header(Location: /stuff-sharing/updateParticular.php");
+  }
+
+?>
+
+<body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="/stuff-sharing/welcome.php"><?php echo $row[0]. " " .$row[1] ?></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav">
+						<li><a href="myitem.php">My Items</a></li>
+						<li><a href="additem.php">Add Item</a></li>
+					</ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="/stuff-sharing/logout.php/">Logout</a></li>
+          </ul> 
+        </div>
+      </div>
+    </nav>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-4 col-md-offset-4">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Update Particulars Page</h3>
+        </div>
+        <div class="panel-body">
+          <?php
+            if(isset($message))
+            {
+              echo '<div class="alert alert-danger" role="alert">
+                      <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                      <span class="sr-only">Error:</span>' . 
+                      $message .
+                    '</div>';
+            }
+          ?>
+          <form action="updateParticular.php" method="post">
+            <fieldset>
+              <div class="form-group">
+                <input class="form-control" id="user-fname" name="userFName" type="text" placeholder="User FName" value = "<?php echo $userFName;?>" autofocus>
+              </div>
+              <div class="form-group">
+                <input class="form-control" id="user-lname" name="userLName" type="text" placeholder="User LName" value = "<?php echo $userLName;?>">
+              </div>
+			  <div class="form-group">
+                <input class="form-control" id="user-password" name="userPassword" type="text" placeholder="User Password" value = "<?php echo $userPassword;?>">
+              </div>
+			  <div class="form-group">
+                <input class="form-control" id="user-dob" name="userDOB" type="text" placeholder="User DOB" value = "<?php echo $userDOB;?>">
+              </div>
+			                
+              <div class="form-group">
+                <input class="form-control" name="action" type="hidden" value="updateid" />
+              </div>
+              <button type="submit" class="btn btn-success btn-block">Update</button>
+
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
--- welcome.php
+++ welcome.php
@@ -20,10 +20,13 @@
     //get archived items
     $email = pg_escape_string($connection,$_SESSION['key']);
     $archivedItems = pg_query($connection,"SELECT l.itemName,l.itemId,l.itemCategory,l.itemDescription FROM ItemList l WHERE l.itemId NOT IN (SELECT a.itemId FROM Advertise a) ORDER BY itemName ASC") or die('Query failed:'.pg_last_error());
-		//get advertised items
-		$advertisements = pg_query($connection,"SELECT i.itemName,i.itemCategory,u.firstName,u.lastName,a.minimumBidPoint,i.itemId from Advertise a, ItemList i, Users u where 
-				a.itemid = i.itemid and i.owneremail = u.email and i.owneremail <> '".$email."'") or die('Query failed:'.pg_last_error());
-		
+	//get advertised items
+	$advertisements = pg_query($connection,"SELECT i.itemName,i.itemCategory,u.firstName,u.lastName,a.minimumBidPoint,i.itemId from Advertise a, ItemList i, Users u where 
+	a.itemid = i.itemid and i.owneremail = u.email and i.owneremail <> '".$email."'") or die('Query failed:'.pg_last_error());		
+	//get particulars
+	$particulars = pg_query($connection,"SELECT u.firstname, u.lastname, u.password, u.dob, u.email FROM users u WHERE u.email = '".$email."'") 
+	or die ('Query failed: '.pg_last_error());
+	
 		if(isset($_POST['itemid']))
 		{
 		$itemid = pg_escape_string($connection,$_POST['itemid']);
@@ -39,6 +42,13 @@
 			{
 
 			}
+		}
+		
+		if(isset($_POST['updateid']))
+		{
+		$updateid = $_POST['updateid'];
+		$_SESSION['updateid'] = $updateid;
+		header("Location: /stuff-sharing/updateParticular.php");
 		}
 		
 		if(isset($_POST['biditemid']))
@@ -98,6 +108,33 @@
     </div>
 
     <div class="container">
+	<div class="accordionSection" id="updateParticulars"><h3>Update Particulars</h3>				
+					<div class="table-responsive">
+					<table class="table table-striped table-bordered table-list">
+					<thead>
+						<tr>
+						<th>First Name</th> <th>Last Name</th> <th>Password</th> <th>DOB</th> <th>Email</th> 
+						</tr>
+					</thead>
+					<tbody>
+					<?php
+						while($row = pg_fetch_row($particulars)){
+							echo "\t<tr>\n";
+							echo "\t\t<td>$row[0]</td>\n";
+							echo "\t\t<td>$row[1]</td>\n";
+							echo "\t\t<td>$row[2]</td>\n";
+							echo "\t\t<td>$row[3]</td>\n";
+							echo "\t\t<td>$row[4]</td>\n";							
+							echo "\t\t<td><form action=\"welcome.php\" method=\"post\">";
+							echo "<input type=\"hidden\" name=\"updateid\" value=\"".$row[4]."\"/>";
+							echo "<button type=\"submit\" class=\"btn btn-success\">Update</button></form></td>\n";
+							echo "\t</tr>\n";
+						}											
+					?>
+					</tbody>
+					</table>
+					</div>
+				</div>
         <div class="accordionSection" id="ongoingTransaction"><h3>Ongoing Transactions</h3><div><p>To be implemented</p></div></div>
         <div class="accordionSection" id="advertisingItems"><h3>Advertisements</h3>				
 					<div class="table-responsive">
